53b163e48723b5f381330f146106af519bffdca5
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-09-11 12:43:42 +0000

    Add zones

--- server.py
+++ server.py
@@ -8,12 +8,23 @@ fr_api = FlightRadar24API()
 @app.route('/get_flights')
 def get_flights():
   flights = fr_api.get_flights()
-  # You can process the flights data as needed here
   return jsonify(flights)
+
+
+@app.route('/get_airports')
+def get_airports():
+  airports = fr_api.get_airports()
+  print(airports)
+  return airports
 
 
 @app.route('/get_airlines')
 def get_airlines():
   airlines = fr_api.get_airlines()
-  # You can process the airlines data as needed here
   return jsonify(airlines)
+
+
+@app.route('/get_zones')
+def get_zones():
+  zones = fr_api.get_zones()
+  return jsonify(zones)
